91195a9965683f36cf61d2e29ceeacbe39e425d1
LucaLeonardelliPixel <luca.leonardelli1@gmail.com>
2025-11-13 16:25:05 +0100

    Bounded fixed

--- app/components/Bounded.tsx
+++ app/components/Bounded.tsx
@@ -11,13 +11,13 @@ type BoundedProps = {
 const Bounded = React.forwardRef<HTMLDivElement, BoundedProps>(
     ({as: Comp = "section", className, children, ...restProps}, ref) => {
         return (
-            <Comp ref={ref} className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
+            <section ref={ref} className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
             {...restProps}>
                 <div className="mx-auto w-full max-w-7xl">
                     {children}
                 </div>
 
-            </Comp>
+            </section>
 
         )
     }
